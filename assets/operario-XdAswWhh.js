1e83df2b2f4e0643b4806cc1b5df038f02b18347
juanHerrera <portador1051@gmail.com>
2025-06-30 01:20:03 -0400

    Updates

--- assets/operario-XdAswWhh.js
+++ assets/operario-XdAswWhh.js
@@ -1,4 +1,4 @@
-import{m as n}from"./maquinas-gTVh8Vsc.js";const o=localStorage.getItem("activeUser")?localStorage.getItem("activeUser"):window.location.href="../index.html",e=JSON.parse(o),a=n.getAll().map(t=>t.name===e.maquina&&{...e,...t}).find(t=>t);document.body.innerHTML+=`
+import{m as n}from"./maquinas-DQrjGjQt.js";const o=localStorage.getItem("activeUser")?localStorage.getItem("activeUser"):window.location.href="../index.html",e=JSON.parse(o),a=n.getAll().map(t=>t.name===e.maquina&&{...e,...t}).find(t=>t);document.body.innerHTML+=`
     <h2>Operario: ${a.name}</h2>
     <p>Maquina: ${a.maquina}</p>
     <p>Estado: ${a.status}</p>
